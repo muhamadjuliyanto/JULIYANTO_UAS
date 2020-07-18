9c88272b6abcb62495c16bef3b9601160b8865ad
muhamadjuliyanto <62062406+muhamadjuliyanto@users.noreply.github.com>
2020-07-18 20:28:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>pendataan Covid 19 | Log in</title>
+  <title>Login Sistem</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href=""><b>Sistem</b>DATA COVID 19</a>
+    <a href=""><b>DATA COVID 19</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
